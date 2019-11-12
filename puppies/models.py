cc1592461a82fa34578a2038cb4c3da83d7ed364
Mohit Khaitan <khaitanmkkmohit@gmail.com>
2019-11-12 14:26:04 +0530

    Written test for Puppy Model

--- puppies/models.py
+++ puppies/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Puppy(models.Model):
+    """
+    Puppy Model
+    Defines the attributes of a puppy
+    """
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_breed(self):
+        """
+        To get the breed of the puppy
+        :return: Breed form Puppy model
+        """
+        return self.name + ' belongs to ' + self.breed + ' breed.'
+
+    def __repr__(self):
+        """
+        To view the representation of the model
+        :return: Name from Puppy model
+        """
+        return self.name + ' is added'
+
+
+
---
+++ puppies/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from puppies.models import Puppy
+
+
+class PuppyTest(TestCase):
+    """ Test module for Puppy model"""
+
+    def setUp(self) -> None:
+        """ Adding dummy entries to the table """
+        Puppy.objects.create(name='Casper', age=3, breed='Bull Dog', color='Black')
+        Puppy.objects.create(name='Muffin', age=1, breed='Gradane', color='Brown')
+
+    def test_puppy_breed(self):
+        """ Test for Puppy breed"""
+        puppy_casper = Puppy.objects.get(name='Casper')
+        puppy_muffin = Puppy.objects.get(name='Muffin')
+
+        self.assert_(puppy_casper.get_breed(), 'Casper belongs to Bull Dog breed.')
+        self.assert_(puppy_muffin.get_breed(), 'Muffin belongs to Gradane breed')
+        
